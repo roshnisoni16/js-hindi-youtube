f8fe0b3bfec36d95f86b6476cd6639fdb097d04b
roshnisoni16 <roshnisoni.ngd@gmail.com>
2024-09-17 06:14:17 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "hitesh";
+//score = undefined => NaN (Not a Number)
+//score = null=>0
+//let score = true =>
+//let score = "33abc" // type of string data type
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false =>0
+*/
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 = false
+//"" => false
+//"hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
